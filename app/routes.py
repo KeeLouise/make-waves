e2b519951d528109ba57a0cf13b7ba3c2842a8e2
Kiera Reidy <keelouise@yahoo.com>
2025-05-02 10:02:39 +0100

    Basic home template and app files. KR 02/05/2025

---
+++ app/routes.py
@@ -0,0 +1,6 @@
+from flask import render_template, request, redirect, url_for, session, flash, jsonify
+
+def register_routes(app):
+   @app.route('/')
+   def home():
+    return render_template('home.html') 
