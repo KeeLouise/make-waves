bdabb3471cb3f3e80bca849bf6a0cdd46f61e667
Kiera Reidy <keelouise@yahoo.com>
2025-05-03 22:57:41 +0100

    Further templates added. KR 03/05/2025

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,12 @@ def register_routes(app):
 
     @app.route('/services')
     def services():
-        return render_template('services.html') 
+        return render_template('services.html')
+    
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+    
+    @app.route('/book')
+    def book():
+        return render_template('book.html')
