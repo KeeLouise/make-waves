8c3b41d9aac0bd3f9c5674085757031118d3d61b
Kiera Reidy <keelouise@yahoo.com>
2025-05-04 20:54:27 +0100

    Change to init file as issues with css files loading. KR 04/05/2025

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from app.routes import register_routes  # import the route registration function
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 
     # Secret key for sessions and flash messages - KR 28/03/2025
     app.config['SECRET_KEY'] = 'NEBULA_2025'
