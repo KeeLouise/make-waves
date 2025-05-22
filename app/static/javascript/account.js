4a1b652b93393534088e30dfeb60c2b2285c4809
Kiera Reidy <keelouise@yahoo.com>
2025-05-22 15:35:19 +0100

    Account javascript added for flash message handling. KR 22/05/2025

---
+++ app/static/javascript/account.js
@@ -0,0 +1,8 @@
+// Automatically fade out alerts after 4 seconds - KR 22/05/2025
+setTimeout(function () {
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(function (alert) {
+      const bsAlert = new bootstrap.Alert(alert);
+      bsAlert.close();
+    });
+}, 3000);
