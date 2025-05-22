bfaab8ad6ade0045fe626f4c17137733997dc862
Kiera Reidy <keelouise@yahoo.com>
2025-05-22 11:12:59 +0100

    Addition of docstring for clarity. KR 22/05/2025

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,7 @@
+"""
+Initialise flask extensions without an app instance.
+These will be tied to the app in create_app() - KR 22/05/2025
+"""
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
