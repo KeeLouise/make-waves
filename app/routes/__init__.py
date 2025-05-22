57a530f5ea77290defc70779e2ec9b9c78d8b313
Kiera Reidy <keelouise@yahoo.com>
2025-05-22 12:27:16 +0100

    Blueprint updates. KR 22/05/2025

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,26 +1,6 @@
-from flask import Flask
-from app.routes import register_routes
-from app.extensions import db, bcrypt, login_manager
-from flask_wtf import CSRFProtect
-csrf = CSRFProtect()
+from .main_routes import main_bp
+from .auth_routes import auth_bp
 
-def create_app():
-    app = Flask(__name__,
-                static_folder='static',
-                template_folder='templates')
-
-    app.config['SECRET_KEY'] = 'MWAVES_2025'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://makewaves_user:Mwaves2025***@localhost/makewaves_db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    csrf.init_app(app)
-
-    # Initialize extensions
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-    register_routes(app)
-
-    return app
+def register_routes(app):
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
