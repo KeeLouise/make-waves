86c7f7a1e39c6f755c3b4060c4b2862d474c3124
Kiera Reidy <keelouise@yahoo.com>
2025-05-25 18:05:28 +0100

    Updated to handle profile image uploading. KR 25/05/2025

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,22 @@ from flask import Flask
 from app.routes import register_routes
 from app.extensions import db, bcrypt, login_manager
 from flask_wtf import CSRFProtect
+from app.config import Config
+
 csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__,
                 static_folder='static',
                 template_folder='templates')
-
-    app.config['SECRET_KEY'] = 'MWAVES_2025'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://makewaves_user:MWaves2025***@localhost:5432/makewaves_db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    app.config.from_object(Config)
 
     csrf.init_app(app)
-
-    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.account'
 
     register_routes(app)
 
