ffa75e2e3f6a0faf5929d7b8f93fb849335da849
Kiera Reidy <keelouise@yahoo.com>
2025-05-25 18:04:50 +0100

    Updated to handle profile image uploads. KR 25/05/2025

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from app.extensions import db, login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -7,10 +8,12 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
-
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     first_name = db.Column(db.String(150), nullable=False)
     surname = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    profile_image = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
