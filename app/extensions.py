08b7e8b07d237b96fdd1db06e55793eb06981598
Kiera Reidy <keelouise@yahoo.com>
2025-06-04 15:47:27 +0100

    Added Flask-Migrate for db updates. KR 04/06/2025

--- app/extensions.py
+++ app/extensions.py
@@ -1,11 +1,9 @@
-"""
-Initialise flask extensions without an app instance.
-These will be tied to the app in create_app() - KR 22/05/2025
-"""
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+migrate = Migrate()
