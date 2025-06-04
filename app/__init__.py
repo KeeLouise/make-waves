d8f2b89a23b7720ed70b78d17b700e67dd169427
Kiera Reidy <keelouise@yahoo.com>
2025-06-04 15:03:23 +0100

    Cloudinary setup for profile images. KR 04/06/2025

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.routes import register_routes
 from app.extensions import db, bcrypt, login_manager
 from flask_wtf import CSRFProtect
 from app.config import Config
+import cloudinary
 
 csrf = CSRFProtect()
 
@@ -12,6 +13,13 @@ def create_app():
                 template_folder='templates')
     
     app.config.from_object(Config)
+
+     #cloudinary setup - 04/06/2025
+    cloudinary.config(
+        cloud_name=app.config['CLOUDINARY_CLOUD_NAME'],
+        api_key=app.config['CLOUDINARY_API_KEY'],
+        api_secret=app.config['CLOUDINARY_API_SECRET']
+    )
 
     csrf.init_app(app)
     db.init_app(app)
