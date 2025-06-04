e6c792a3ad3868125f4e61586d180f61dcd21d8b
Kiera Reidy <keelouise@yahoo.com>
2025-06-04 15:03:28 +0100

    Cloudinary setup for profile images. KR 04/06/2025

--- app/config.py
+++ app/config.py
@@ -16,3 +16,8 @@ class Config:
     # profile image uploads - KR 01/06/2025
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
     UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')
+
+    # cloudinary credentials - KR 04/06/2025
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
