926e6fe2feea938d44a84337ec7f76f06d9a4a55
Kiera Reidy <keelouise@yahoo.com>
2025-06-05 16:48:14 +0100

    Making hamburger menu functional. KR 05/06/2025

---
+++ app/static/javascript/home.js
@@ -0,0 +1,9 @@
+ document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    hamburger.addEventListener('click', function () {
+      mobileNav.classList.toggle('active');
+      hamburger.classList.toggle('open');
+    });
+  });
