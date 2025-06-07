791eb065f705980c0116eb54966f49e35c17f086
Kiera Reidy <keelouise@yahoo.com>
2025-06-07 14:56:37 +0100

    Implementing custom error messages. KR 07/06/2025

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.routes import register_routes
 from app.extensions import db, bcrypt, login_manager, migrate
+from app.errors import register_error_handlers
 from flask_wtf import CSRFProtect
 from app.config import Config
 import cloudinary
@@ -8,27 +9,24 @@ import cloudinary
 csrf = CSRFProtect()
 
 def create_app():
-    app = Flask(__name__,
-                static_folder='static',
-                template_folder='templates')
+    app = Flask(__name__, static_folder='static', template_folder='templates')
     
     app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.account'
 
-     #cloudinary setup - 04/06/2025
     cloudinary.config(
         cloud_name=app.config['CLOUDINARY_CLOUD_NAME'],
         api_key=app.config['CLOUDINARY_API_KEY'],
         api_secret=app.config['CLOUDINARY_API_SECRET']
     )
 
-    csrf.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.account'
-
     register_routes(app)
+    register_error_handlers(app)  # Register custom errors - KR 07/06/2025
 
     return app
